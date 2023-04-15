efbc9a151a0d602689977132b340eaa2dbfbf272
Tigran <davidyan.tigran.git@gmail.com>
2023-04-15 13:47:30 +0300

    Add Dokerfile and order in endpoints

---
+++ Dockerfile
@@ -0,0 +1,24 @@
+FROM python:3.9
+
+RUN mkdir /app
+
+WORKDIR /app
+
+COPY requirements.txt .
+
+RUN pip install -r requirements.txt
+
+COPY . .
+
+WORKDIR /src
+
+CMD gunicorn main:app --w
+
+RUN chmod a+x docker/*.sh
+
+
+RUN alembic upgrade head
+
+WORKDIR src
+
+CMD gunicorn main:app --workers 1 --worker-class uvicorn.workers.UvicornWorker --bind=0.0.0.0:8000
--- src/main.py
+++ src/main.py
@@ -12,13 +12,13 @@ app = FastAPI()
 
 @app.get("/authors", response_model=list[Author])
 async def get_authors(offset: int = 0, limit: int = 10, session: AsyncSession = Depends(get_async_session)):
-    query = select(author).limit(limit).offset(offset)
+    query = select(author).order_by(author.c.name).limit(limit).offset(offset)
     result = (await session.execute(query)).all()
     return [Author(id=a[0], name=a[1], username=a[2], job=a[3], photo=a[4]) for a in result]
 
 
 @app.get("/publications/{authorid}", response_model=list[Publication])
 async def get_publications(author_id: int, offset: int = 0, limit: int = 10,  session: AsyncSession = Depends(get_async_session)):
-    query = select(publication).where(publication.c.author_id == author_id).order_by().limit(limit).offset(offset)
+    query = select(publication).where(publication.c.author_id == author_id).order_by(publication.c.date).limit(limit).offset(offset)
     result = (await session.execute(query)).all()
     return [Publication(id=p[0], title=p[1], date=p[2], author_id=p[3], description=p[4]) for p in result]
